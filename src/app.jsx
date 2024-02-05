e813b29fa204771bb210a431928d3d990626f71a
Simon Bertram <simonbertram@hotmail.com>
2024-02-05 23:36:11 +0000

    Add UserForm and UserTable

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,12 @@
-import { useState } from "preact/hooks";
+import { UserForm } from "./components/UserForm";
+import { UserTable } from "./components/UserTable";
 import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div class="card container mx-auto mt-7">
-        <button
-          className="bg-slate-500 rounded-lg p-3 text-white shadow-md"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div className="container mx-auto mt-12">
+      <UserForm />
+      <UserTable />
+    </div>
   );
 }
